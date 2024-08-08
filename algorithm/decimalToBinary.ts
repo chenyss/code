501b2a75e63e7fb5210c1a944306b6eb369cc00a
joekylam <14607721+joekylam@user.noreply.gitee.com>
2024-08-08 15:58:15 +0800

    decimalToBinary

---
+++ algorithm/decimalToBinary.ts
@@ -0,0 +1,17 @@
+import ArrayStack from "../stack/ArrayStack"
+
+function decimalToBinary(decimal:number):string{
+    const stack = new ArrayStack<number>()
+    while(decimal > 0){
+        const data = decimal % 2
+        stack.push(data)
+        decimal = Math.floor(decimal / 2)
+    }
+    let binary = ''
+    while(stack.size() > 0){
+        binary += stack.pop()
+    }
+    return binary
+}
+
+export default decimalToBinary
--- stack/ArrayStack.ts
+++ stack/ArrayStack.ts
@@ -1,23 +1,27 @@
-class ArrayStack {
+import IStack from "../type/IStack";
+
+class ArrayStack<T> implements IStack<T> {
   private data: any[] = [];
 
-  push(element: any): void {
+  push(element: T): void {
     this.data.push(element);
   }
 
-  pop(): any {
+  pop(): T {
     return this.data.pop();
   }
 
-  peek(): any {
+  peek(): T {
     return this.data[this.data.length - 1];
   }
 
   isEmpty(): boolean {
     return this.data.length === 0;
   }
+
   size(): number {
     return this.data.length;
   }
 }
+
 export default ArrayStack;
